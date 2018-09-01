5cc19524414b6ab0a565dadcbe367ea11ad04f11
Rohan Kalgutkar <hello@rohankalgutkar.in>
2018-09-01 12:06:30 +0530

    init

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET login page. */
+//  --------------------------------------- GET Routes --------------------------------------- //
+// Home page
+router.get('/', (req, res, next) => {
+  res.render('index', {
+    title: 'Home | WMMA'
+  });
+});
+router.get('/index', (req, res, next) => {
+  res.redirect('/');
+});
+
+// Login page
 router.get('/login', (req, res, next) => {
   res.render('login', {
     title: 'Login | WMMA'
   });
 });
 
-// POST login page
-router.post('/login', (req, res) => {
-  console.log('Form data: ' + JSON.stringify(req.body));
-  res.redirect('/dashboard');
+// Register page
+router.get('/register', (req, res, next) => {
+  res.render('register', {
+    title: 'Register | WMMA'
+  });
 });
 
-
-// Get Dashboard / home page
+// Dashboard
 router.get('/dashboard', (req, res, next) => {
   res.render('dashboard', {
     title: 'Dashboard | WMMA',
     emailid: 'hello@rohankalgutkar.in'
   });
+});
+
+//  --------------------------------------- POST Routes --------------------------------------- //
+// Login
+router.post('/login', (req, res) => {
+  console.log('Login form data: ' + JSON.stringify(req.body));
+  res.redirect('/dashboard');
+});
+
+// Register
+router.post('/register', (req, res) => {
+  console.log('Register form data: ' + JSON.stringify(req.body));
+  res.redirect('/dashboard');
 });
 
 module.exports = router;
